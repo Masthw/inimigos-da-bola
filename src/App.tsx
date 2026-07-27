522f6dafbe470a556c4553af84929bf36db320c4
SilveiraDev <silveira.eduardo@senairs.org.br>
2026-07-27 11:30:39 -0300

    add new pages to App routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import ForgotPassword from './pages/ForgotPassword'
+import Register from './pages/Register'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
